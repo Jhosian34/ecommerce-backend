7c57a85a836c87236ee34153d2a994037490df98
jhosian <jhosiansalazar@gmail.com>
2025-08-30 07:24:49 -0300

    mejorando rutas

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,24 +1,22 @@
 const express = require("express");
 const router = express.Router();
-const userController = require("../controllers/user.controller")
-const auth = require("../middlewares/auth.middleware")
-const isAdmin = require("../middlewares/admin.middleware")
+const userController = require("../controllers/user.controller");
+const auth = require("../middlewares/auth.middleware");
+const isAdmin = require("../middlewares/admin.middleware");
 const upload = require('../middlewares/upload.middleware');
 
+// Rutas específicas primero
 router.post("/login", userController.loginUser);
 router.get('/me', auth, userController.getCurrentUser);
+router.post('/register', upload.single('avatar'), userController.registerUser);
+
+// Luego las rutas generales
 router.get("/", userController.getUsers);
 router.post("/", userController.createUsers);
 
-
+// Rutas dinámicas al final
 router.get("/:id", userController.getUserById);
 router.delete("/:id", [auth, isAdmin], userController.deleteUserById);
 router.put("/:id", [auth], userController.updateUserById);
-router.post(
-    '/register',
-    upload.single('avatar'),
-    userController.registerUser
-);
 
-
-module.exports = router
+module.exports = router;
